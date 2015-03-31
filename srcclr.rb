22866a21b1f04f9d3aff7f81b81bafda0c297001
Jason Nichols <jason@kickroot.com>
2015-03-31 13:53:08 -0400

    ooh

--- srcclr.rb
+++ srcclr.rb
@@ -7,8 +7,8 @@ class Srcclr < Formula
 
   def install
   	prefix.install "srcclr-console.jar"
-  	bin.install "console.sh"
-#  	bin.install_symlink Dir["#{libexec}/*"]
+#  	bin.install "console.sh"
+  	bin.install_symlink Dir["console.sh"]
 #    system "./configure", "--disable-debug", "--disable-dependency-tracking",
 #                          "--prefix=#{prefix}"
 #    system "make install"
