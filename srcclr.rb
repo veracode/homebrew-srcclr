cd300002b36b18d07ed08df5659d8ca6dd126717
Jason Nichols <jason@kickroot.com>
2015-03-31 13:31:52 -0400

    ooh

--- srcclr.rb
+++ srcclr.rb
@@ -7,7 +7,7 @@ class Srcclr < Formula
 
   def install
   	prefix.install Dir["*"]
-  	bin.install "console.sh"
+  	bin.install Dir["consonle.sh"]
 #  	bin.install_symlink Dir["#{libexec}/*"]
 #    system "./configure", "--disable-debug", "--disable-dependency-tracking",
 #                          "--prefix=#{prefix}"
