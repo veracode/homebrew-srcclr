2e15876b2c078bd1db364c7290e1f968fa281a49
Jason Nichols <jason@kickroot.com>
2015-03-31 17:34:57 -0400

    New Hash

--- srcclr.rb
+++ srcclr.rb
@@ -3,9 +3,11 @@ require "formula"
 class Srcclr < Formula
   homepage "https://sourceclear.com"
   url "https://download.sourceclear.com/console/srcclr-0.6.1.zip"
-  sha256 "9ef81de980b54ae1eebf745792e91a7165d5e77685de341d4763c52baa8f4e9e"
+  sha256 "03b93588a11ffdd4c1948c0f96e755374d3d3917be545cf6b36651d337c82e9e"
 
   def install
+  	inreplace "srcclr", "##PREFIX##", "#{prefix}"
+
   	prefix.install "srcclr-console.jar"
   	bin.install "brew/srcclr"
 #  	bin.install_symlink "console.sh"
