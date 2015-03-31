cb87a552e460f6fccf440b4c5d837c71b017cb39
Jason Nichols <jason@kickroot.com>
2015-03-31 14:16:24 -0400

    Added new executable

--- srcclr.rb
+++ srcclr.rb
@@ -7,7 +7,7 @@ class Srcclr < Formula
 
   def install
   	prefix.install "srcclr-console.jar"
-  	bin.install "console.sh"
+  	bin.install "brew/srcclr"
 #  	bin.install_symlink "console.sh"
 #    system "./configure", "--disable-debug", "--disable-dependency-tracking",
 #                          "--prefix=#{prefix}"
