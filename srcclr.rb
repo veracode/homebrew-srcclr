c8e31fda0d530a2904cee2e7dfd6b8b54f872510
Jason Nichols <jason@kickroot.com>
2015-04-06 11:33:23 -0400

    Updating to 0.7.0 release

--- srcclr.rb
+++ srcclr.rb
@@ -2,17 +2,12 @@ require "formula"
 
 class Srcclr < Formula
   homepage "https://sourceclear.com"
-  url "https://download.sourceclear.com/console/srcclr-0.6.1.zip"
-  sha256 "03b93588a11ffdd4c1948c0f96e755374d3d3917be545cf6b36651d337c82e9e"
+  url "https://download.sourceclear.com/console/srcclr-0.7.0.zip"
+  sha256 "6fdae5ff1adf92c5f99821cf1aceae8144686bf71350ccf0bbabb526da14de10"
 
   def install
   	inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
-
   	prefix.install "srcclr-console.jar"
   	bin.install "brew/srcclr"
-#  	bin.install_symlink "console.sh"
-#    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-#                          "--prefix=#{prefix}"
-#    system "make install"
   end
 end
