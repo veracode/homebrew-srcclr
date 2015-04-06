09cf537f5311fcb1738539d8f37fcac51db0770c
Jason Nichols <jason@kickroot.com>
2015-04-06 11:35:29 -0400

    Fixing bad url link

--- srcclr.rb
+++ srcclr.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Srcclr < Formula
   homepage "https://sourceclear.com"
-  url "https://download.sourceclear.com/console/srcclr-0.7.0.zip"
+  url "https://download.sourceclear.com/console/srcclr-console-0.7.0.zip"
   sha256 "6fdae5ff1adf92c5f99821cf1aceae8144686bf71350ccf0bbabb526da14de10"
 
   def install
