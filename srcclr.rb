24e18579da030cf63d32faf0263afbc1c56b0ca4
Jason Nichols <jason@kickroot.com>
2015-06-24 11:17:36 -0400

    Updating to 0.8.8

--- srcclr.rb
+++ srcclr.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Srcclr < Formula
   homepage "https://sourceclear.com"
-  url "https://download.srcclr.com/console/srcclr-console-0.8.5.zip"
-  sha256 "f3a3df9fa68698ca6475c97f84ea2ca7290052fdb2f7f561775c44af8ea65f26"
+  url "https://download.srcclr.com/console/srcclr-console-0.8.8.zip"
+  sha256 "c019bee61f482416f48b80bfaa02731227c0f962c750e032bcb6a3017e046911"
 
   def install
   	inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
