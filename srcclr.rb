d856ec0b7674342f96e8050259280634fc6e7475
Jason Nichols <jason@kickroot.com>
2015-07-24 13:48:22 -0400

    Updating to latest console

--- srcclr.rb
+++ srcclr.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Srcclr < Formula
   homepage "https://srcclr.com"
-  url "https://download.srcclr.com/console/srcclr-console-0.10.0.zip"
-  sha256 "8a8a5758694d407010134c6e1783776582f54bcae5882805095377351971a1aa"
+  url "https://download.srcclr.com/console/srcclr-console-0.10.2.zip"
+  sha256 "bfc637cbfdc0ff8b61f48972354a9df1dc7a8870e1f3673d2d60d9c97707f331"
 
   def install
   	inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
