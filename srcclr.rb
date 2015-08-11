3329cbcf19756d6f327daac32183e47788231334
Jason Nichols <jason@kickroot.com>
2015-08-11 14:52:45 -0400

    Moving to 0.12.2

--- srcclr.rb
+++ srcclr.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Srcclr < Formula
   homepage "https://srcclr.com"
-  url "https://download.srcclr.com/console/srcclr-console-0.12.1.zip"
-  sha256 "2c7a2f6585c614f1e0e701f7b4ba5c1f02551cf6026ace06f27d766e30b8dbf0"
+  url "https://download.srcclr.com/console/srcclr-console-0.12.2.zip"
+  sha256 "04ecbe5d0dd8fd694cb31077c8170158cb1213bfcf2a6b4ea8669fe1e1bf1519"
 
   def install
   	inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
