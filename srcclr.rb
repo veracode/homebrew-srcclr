dbeb151867e77002654d31144bb4314585a61c51
Matthew L Daniel <mdaniel@srcclr.com>
2015-09-23 22:39:14 -0700

    Correct brew audit complaints
    * Brew mandates indentation with spaces, not tabs
    * Brew audit correctly noticed that a string literal containing an interpolation of a variable is in fact that variable
    * Brew formulae no longer need `require "formula"`
    
    After these changes, we get a "no offenses detected" green bill of health

--- srcclr.rb
+++ srcclr.rb
@@ -1,13 +1,11 @@
-require "formula"
-
 class Srcclr < Formula
   homepage "https://srcclr.com"
   url "https://download.srcclr.com/console/srcclr-console-0.13.1.zip"
   sha256 "13ce6fdbae70a09e294c2b2230bafa4a127b85990c3bef39da725eefb8f2f069"
 
   def install
-  	inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
-  	prefix.install "srcclr-console.jar"
-  	bin.install "brew/srcclr"
+    inreplace "srcclr", "##PREFIX##", prefix
+    prefix.install "srcclr-console.jar"
+    bin.install "brew/srcclr"
   end
 end
