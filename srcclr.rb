d63c5b874a4c772e8c6bf2e079567efb07af6e66
Mark Winterrowd <mww@srcclr.com>
2016-03-30 10:49:59 -0700

    Updated to srcclr terminal version 1.2.8

--- srcclr.rb
+++ srcclr.rb
@@ -3,8 +3,8 @@ require "formula"
 class Srcclr < Formula
   desc "The Terminal interface to the SRC:CLR platform"
   homepage "https://srcclr.com"
-  url "https://download.srcclr.com/console/srcclr-console-1.2.7.tgz"
-  sha256 "51c91041c622cc1f75a57c9dadedfc70497e99a7b1dd90efeee2ab2a64db78cc"
+  url "https://download.srcclr.com/console/srcclr-console-1.2.8.tgz"
+  sha256 "9ea0c391ef9d77adf12aad03ab7ac3c0df04b1d530b8d2c021a7f769f3c62132"
 
   def install
     inreplace "srcclr", "##PREFIX##", prefix
@@ -14,7 +14,7 @@ class Srcclr < Formula
   end
 
   test do
-    tar_version = "1.2.7+b10c389a.25"
+    tar_version = "1.2.8+1b6bc62a.27"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
