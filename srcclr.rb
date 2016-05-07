e04d161dd795ecb38940d6cbf4fe62febc29d882
Matthew L Daniel <mdaniel@srcclr.com>
2016-05-07 12:47:41 -0700

    Version bump to 1.9.15

--- srcclr.rb
+++ srcclr.rb
@@ -1,20 +1,17 @@
 require "formula"
 
 class Srcclr < Formula
-  desc "The Terminal interface to the SRC:CLR platform"
+  desc "The command-line interface to the SourceClear platform"
   homepage "https://srcclr.com"
-  url "https://download.srcclr.com/console/srcclr-console-1.2.10.tgz"
-  sha256 "71d2311c12649d929dacf5e7b3b67de102de0a5ed68ae45abdfbd56e9720df6c"
+  url "https://download.srcclr.com/srcclr-1.9.15.tgz"
+  sha256 "b3f4d47eb4e61aa7dfc9c480bbdbcf188c8523df7d942e1c6a52f37a5a19ecba"
 
   def install
-    inreplace "srcclr", "##PREFIX##", prefix
-    prefix.install "srcclr-console.jar"
-    bin.install "srcclr"
-    man1.install "usr/share/man/man1/srcclr.1"
+    bin.install "bin/srcclr"
   end
 
   test do
-    tar_version = "1.2.10+fb1551e7.29"
+    tar_version = "1.9.15+d43567aa.23"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
