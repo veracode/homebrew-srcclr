beef6bfb4d1b17d8313ab35d032d2fb6cdb882bf
Matthew L Daniel <mdaniel@srcclr.com>
2016-05-07 16:42:32 -0700

    Version bump to 1.9.17

--- srcclr.rb
+++ srcclr.rb
@@ -3,15 +3,15 @@ require "formula"
 class Srcclr < Formula
   desc "The command-line interface to the SourceClear platform"
   homepage "https://srcclr.com"
-  url "https://download.srcclr.com/srcclr-1.9.15.tgz"
-  sha256 "b3f4d47eb4e61aa7dfc9c480bbdbcf188c8523df7d942e1c6a52f37a5a19ecba"
+  url "https://download.srcclr.com/srcclr-1.9.17.tgz"
+  sha256 "040d573df4fd76f8b667972fef459646fc10a56f21e765b79c4036eed58923f1"
 
   def install
     bin.install "bin/srcclr"
   end
 
   test do
-    tar_version = "1.9.15+d43567aa.23"
+    tar_version = "1.9.17+ef849d30.25"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
