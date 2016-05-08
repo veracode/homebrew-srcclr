3559d92dd680a6b27e5a2aa9669cc705d7fea83a
Matthew L Daniel <mdaniel@srcclr.com>
2016-05-07 23:33:56 -0700

    Correct the sha256sum and tar version

--- srcclr.rb
+++ srcclr.rb
@@ -4,14 +4,14 @@ class Srcclr < Formula
   desc "The command-line interface to the SourceClear platform"
   homepage "https://srcclr.com"
   url "https://download.srcclr.com/srcclr-2.0.2+alpha1.tgz"
-  sha256  "26a82853d6bb4e9332a4da4c436edba2b5b3f023b3db477d5d1dafda1699f192"
+  sha256 "3e8f4ecb02edbefe4710c799caa40c9118aecf46fc1695cec4b57b74a5de1113"
 
   def install
     bin.install "bin/srcclr"
   end
 
   test do
-    tar_version = "1.9.17+ef849d30.25"
+    tar_version = "2.0.2+alpha1.0d11c07d.30"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
