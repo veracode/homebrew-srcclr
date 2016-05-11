b791d31e79f47cdecc07f84939d1980aba2f25ab
Matthew L Daniel <mdaniel@srcclr.com>
2016-05-10 19:22:54 -0700

    Include tar version test output

--- srcclr.rb
+++ srcclr.rb
@@ -12,7 +12,7 @@ class Srcclr < Formula
   end
 
   test do
-    tar_version = "2.0.11+99232eba.40"
+    tar_version = "2.0.13"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
