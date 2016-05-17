29c69741b87f1c5aedf1f830c65e3959f39109ee
Matthew L Daniel <mdaniel@srcclr.com>
2016-05-16 19:37:29 -0700

    Update to new directory structure

--- srcclr.rb
+++ srcclr.rb
@@ -7,12 +7,13 @@ class Srcclr < Formula
   sha256 "d7b587bcdc79303631e763a2af90265485be0d911b5216dfededdf857f3c652f"
 
   def install
-    bin.install "bin/srcclr"
-    man1.install "share/man/man1/srcclr.1"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/srcclr"
+    man1.install libexec/"share/man/man1/srcclr.1"
   end
 
   test do
-    tar_version = "2.0.15"
+    tar_version = "2.1.0"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
