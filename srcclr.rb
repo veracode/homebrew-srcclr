b239044dbadd2f880ce5cd3a2f07f8bfe2812f44
cl4r1ty <paul@srcclr.com>
2016-12-07 11:37:16 -0800

    Move back to 2.7.3

--- srcclr.rb
+++ srcclr.rb
@@ -3,8 +3,8 @@ require "formula"
 class Srcclr < Formula
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
-  url "https://download.srcclr.com/srcclr-2.8.0-macosx.tgz"
-  sha256 "0ba2e896df6dcab4097ce65e67e883d535892ff9dad2a154dc6a911733f326d0"
+  url "https://download.srcclr.com/srcclr-2.7.3-macosx.tgz"
+  sha256 "4887edc9b09e6de33fca94718612f44e9a0af893fe2322d5e8f2c9c63df7f954"
 
   def install
     libexec.install Dir["*"]
@@ -13,7 +13,7 @@ class Srcclr < Formula
   end
 
   test do
-    tar_version = "2.8.0"
+    tar_version = "2.7.3"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
