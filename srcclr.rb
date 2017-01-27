d7b725f3d7b23ce5e375956cace01ee9bc4107b9
Code Signer <jenkins@srcclr.com>
2017-01-26 19:08:32 -0800

    Update srcclr to 2.9.1

--- srcclr.rb
+++ srcclr.rb
@@ -3,8 +3,8 @@ require "formula"
 class Srcclr < Formula
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
-  url "https://download.srcclr.com/srcclr-2.8.6-macosx.tgz"
-  sha256 "5bcb11e89e2629f2a2cf8198f90d916ef58141a29c541c8cbc0d59be5cbd1e0f"
+  url "https://download.srcclr.com/srcclr-2.9.1-macosx.tgz"
+  sha256 "f1f19d3e34391d2fe368463c4c7b9bb43ec455c33b0acef0c58b0672e03ec4ee"
 
   def install
     libexec.install Dir["*"]
@@ -13,7 +13,7 @@ class Srcclr < Formula
   end
 
   test do
-    tar_version = "2.8.6"
+    tar_version = "2.9.1"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
