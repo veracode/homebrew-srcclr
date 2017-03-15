25aa87161ecbe95f7f6d15e840f8362456ecd0ab
Jason Yeo <jasonyeo88@gmail.com>
2017-03-15 13:11:23 +0800

    Release 2.10.7 of the agent

--- srcclr.rb
+++ srcclr.rb
@@ -3,7 +3,7 @@ require "formula"
 class Srcclr < Formula
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
-  url "https://download.srcclr.com/srcclr-2.10.6-macosx.tgz"
+  url "https://download.srcclr.com/srcclr-2.10.7-macosx.tgz"
   sha256 "6210496b0ca59b6a69a8e81c821771ccf09237f424b3db4b1abd9e4ca430a89a"
 
   def install
