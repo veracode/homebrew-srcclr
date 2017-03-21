6032348df69cb7fda2a24f00f9c34bb3e6a93a0b
Jason Yeo <jasonyeo88@gmail.com>
2017-03-21 23:21:55 +0800

    Release version 2.10.9 of srcclr agent

--- srcclr.rb
+++ srcclr.rb
@@ -3,8 +3,8 @@ require "formula"
 class Srcclr < Formula
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
-  url "https://download.srcclr.com/srcclr-2.10.8-macosx.tgz"
-  sha256 "c7769ee7a8c1724b9ea88ad9114b77005c0841e0a147aa68607c9c8ad3ddca90"
+  url "https://download.srcclr.com/srcclr-2.10.9-macosx.tgz"
+  sha256 "abc39e5755c56080face2de17add495ef071f18f34a86610d0de14d2caa1da56"
 
   def install
     libexec.install Dir["*"]
@@ -13,7 +13,7 @@ class Srcclr < Formula
   end
 
   test do
-    tar_version = "2.10.8"
+    tar_version = "2.10.9"
     assert_equal "Version: #{tar_version}", `srcclr --version`.strip
   end
 end
