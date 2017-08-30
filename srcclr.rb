eff74ee5671699b2e35c027479def1551418fa4e
hendychua <hendy@srcclr.com>
2017-08-30 16:30:36 +0800

    Updated to 2.10.29.

--- srcclr.rb
+++ srcclr.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Srcclr < Formula
-  VERSION = "2.10.28"
+  VERSION = "2.10.29"
 
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
   url "https://download.srcclr.com/srcclr-#{VERSION}-macosx.tgz"
-  sha256 "34e30a4aeec6e4dc26a2b29c76537c7736f7b06e8739eeb6ca6df3be178c4f8f"
+  sha256 "5c0d9e3633c0f8ff6319b8b4d8ede63f49c63a77839f907353d9f9948e55a1a7"
 
   def install
     libexec.install Dir["*"]
