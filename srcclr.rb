84f386e584b7c042915f7c199b08544c573f7136
Jenkins User <nobody@srcclr.com>
2018-02-01 11:17:38 +0000

    Updated srcclr.rb for version 2.13.14

--- srcclr.rb
+++ srcclr.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Srcclr < Formula
-  VERSION = "2.13.13"
+  VERSION = "2.13.14"
 
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
   url "https://download.srcclr.com/srcclr-#{VERSION}-macosx.tgz"
-  sha256 "aa814960eb1c1abb063a46c17a0c003f4ebb958800c49d3bd47c7fe387027836"
+  sha256 "604c0fc2dd5c4c78a4a1d87c9f7180eb5c37b9676500897279066340891013c9"
 
   def install
     libexec.install Dir["*"]
