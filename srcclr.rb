08e2d1ee373ebb2e6a0f62755d5d1fcccbaac224
Jenkins User <nobody@srcclr.com>
2019-04-01 04:55:58 +0000

    Updated srcclr.rb for version 3.2.8

--- srcclr.rb
+++ srcclr.rb
@@ -1,12 +1,12 @@
 require "formula"
 
 class Srcclr < Formula
-  VERSION = "3.2.7"
+  VERSION = "3.2.8"
 
   desc "The command-line interface to the SourceClear platform"
   homepage "https://www.sourceclear.com"
   url "https://download.srcclr.com/srcclr-#{VERSION}-macosx.tgz"
-  sha256 "a613909163832af431ad173c385c73270a6607f86af9a4f0a2eee6b796e070aa"
+  sha256 "e0cb5f0228f6d7a78714a689eba943f3bf258ffb8c024f1c27b706f449c48572"
 
   def install
     prefix.install 'THIRD-PARTY.txt'
