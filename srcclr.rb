d81def8fb59d8a00d74a2281c481a1ddf17916d8
Ahmed ElMallah <50939675+Ahmed3lmallah@users.noreply.github.com>
2023-05-01 14:42:25 -0700

    Updating Cask description

--- srcclr.rb
+++ srcclr.rb
@@ -3,7 +3,7 @@ require "formula"
 class Srcclr < Formula
   VERSION = "3.8.28"
 
-  desc "The command-line interface to the SourceClear platform"
+  desc "Command-line interface to the Veracode SourceClear platform"
   homepage "https://www.sourceclear.com"
   url "https://download.srcclr.com/srcclr-#{VERSION}-macosx.tgz"
   sha256 "ffc505c18fde86b18d4e2646210f77a9e90e25ced34eea588364b08709e5a238"
