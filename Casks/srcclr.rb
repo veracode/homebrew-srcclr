7e048944332ab1acf47c39376bbfe511c05cd660
CI/CD User <nobody@veracode.com>
2025-04-01 18:07:41 +0000

    Updated Casks/srcclr.rb for version 3.8.95

--- Casks/srcclr.rb
+++ Casks/srcclr.rb
@@ -1,6 +1,6 @@
 cask "srcclr" do
-  version "3.8.94"
-  sha256 "99a3679c54a0641e05df3d61afd5045c7d22ce65502582af474954c2b375def5"
+  version "3.8.95"
+  sha256 "bc1de20ac45812f650496be5648c86cc0217dfb8b812846d40b24dc152bcbfba"
 
   url "https://download.srcclr.com/srcclr-#{version}-macosx.tgz",
       verified: "download.srcclr.com/"
